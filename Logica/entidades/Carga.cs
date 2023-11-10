d6aa4e0d7fa3cf45186267af2ccaf01589a2b4d1
Nahuel Gamarra <gamarraorlandonahuel@gmail.com>
2023-11-09 22:15:11 -0300

    un operador puede transferir su carga fisica a otro operador

---
+++ Logica/entidades/Carga.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.entidades
+{
+    public class Carga
+    {
+        private static int contadorId = 0;
+        public int Id { get; private set; }
+        public string Descripcion { get; set; }
+        public double Peso { get; set; } = 0;
+        public Carga() { 
+        Id= contadorId++;
+        }
+    }
+}
--- Logica/entidades/Operador.cs
+++ Logica/entidades/Operador.cs
@@ -15,30 +15,74 @@ public abstract class Operador : ElementoMapa
     public double VelocidadOptima { get; }
     public string LocalizacionActual { get; set; }
 
+    public List<Carga> Cargas { get; } = new List<Carga>();
+
     public Operador(string nombre, int fila, int columna, double cargaMaxima) : base(nombre, fila, columna)
     {
+      
         CargaMaxima = cargaMaxima;
         Id = contadorId++;
     }
 
-    public  double CargaMaxima { get; private set; }
+    public double CargaMaxima { get; private set; }
     public abstract void Moverse(double distancia);
-    public virtual string MostrarLocalizacio() {
+    public virtual string MostrarLocalizacio()
+    {
 
         return "";
-    } 
+    }
 
-    public  string MostrarLocalizacion() { 
-      LocalizacionActual= $"Fila {Fila}, Columna {Columna}";
+    public string MostrarLocalizacion()
+    {
+        LocalizacionActual = $"Fila {Fila}, Columna {Columna}";
         return LocalizacionActual;
     }
-    
-        // Otras propiedades y métodos
 
-        public bool EstanEnLaMismaUbicacion(Operador otroOperador)
+    public void TransferirCarga(Carga carga, Operador operador)
+    {
+        if (EstanEnLaMismaUbicacion(operador) && !SuperaPesoMaximo(carga))
         {
-            return this.Fila == otroOperador.Fila && this.Columna == otroOperador.Columna;
+            this.sacarCarga(carga);
+            operador.AgregarCarga(carga);
         }
-    
+        else {
+            Console.WriteLine("No se pudo transferir la carga");
+        }
+          
+    }
+
+    public void sacarCarga(Carga carga)
+    {
+        Cargas.Remove(carga);
+    }
+
+    public void AgregarCarga(Carga carga)
+    {
+        if(!SuperaPesoMaximo(carga))
+        {
+            Cargas.Add(carga);
+        }else
+        {
+            Console.WriteLine("No se pudo agregar la carga debido al peso excedido.");
+        }
+
+
+    }
+    private double ObtenerPesoDeCargaActual()
+    {
+
+        return Cargas.Sum(carga => carga.Peso);
+    }
+
+    private bool SuperaPesoMaximo(Carga carga)
+    {
+        return ObtenerPesoDeCargaActual() + carga.Peso > CargaMaxima;
+    }
+
+    public bool EstanEnLaMismaUbicacion(Operador otroOperador)
+    {
+        return this.Fila == otroOperador.Fila && this.Columna == otroOperador.Columna;
+    }
+
 }
 
