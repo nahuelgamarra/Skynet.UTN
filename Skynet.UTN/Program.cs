622c12e52ec367cef5b829075dd8122ad74007a6
Nahuel Gamarra <gamarraorlandonahuel@gmail.com>
2023-12-01 14:40:53 -0300

    Cosas que hace el programa y autores

--- Skynet.UTN/Program.cs
+++ Skynet.UTN/Program.cs
@@ -1,4 +1,9 @@
-﻿using Logica.entidades;  // Asegúrate de incluir esta línea
+﻿//Autores
+//Angel Gomez
+//Nahuel Gamarra
+
+using Logica.entidades;  // Asegúrate de incluir esta línea
+using Logica.entidades.Logica.entidades;
 using Logica.Operadores;
 
 
@@ -22,7 +27,7 @@ namespace Skynet.UTN
            
             
             Console.WriteLine(operador.ObtenerPesoDeCargaActual()+ " Contengo esta cantidad de cargas"); 
-            /*  
+           
            // Crear un operador específico, por ejemplo, K9
            Cuartel cuartel = new Cuartel(1, 2, mapa);
 
@@ -34,17 +39,13 @@ namespace Skynet.UTN
            cuartel.AgregarElemento(operador2);
            cuartel.AgregarElemento(operador3);
 
-        Mapa mapa2 = Mapa.ObtenerInstanciaExistente();
-
+     
        Console.WriteLine($"Veremos cuantos operadores hay en el cuarte  {cuartel.ListaOperadores.Count}");
 
 
 
-           //   mapa.AgregarElemento(operador, operador.Fila, operador.Columna);
-           // mapa.AgregarElemento(operador2, operador2.Fila, operador2.Columna);
-           //  mapa.AgregarElemento(operador3, operador3.Fila, operador3.Columna);
-           //  mapa.AgregarElemento(cuartel, cuartel.Fila, cuartel.Columna);
-             mapa2.MostrarMapa();
+        
+            // mapa2.MostrarMapa();
            Carga carga = new Carga();
            Carga carga2 = new Carga();
            carga.Descripcion = "carga 1";
@@ -92,7 +93,7 @@ namespace Skynet.UTN
            Console.WriteLine(operador.Bateria.Capacidad + " Y tiene esta bateria");
 
            Console.WriteLine(operador.MostrarLocalizacion()+ "Aca esta el operador luego de moverse");
-         */
+       
 
         }
     }
